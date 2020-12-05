f1378e325191662378108e185334584ed3630673
Hatice Ergün <hadeyici@gmail.com>
2020-12-05 23:18:54 +0300

    db connection added

--- app.js
+++ app.js
@@ -1,41 +1,30 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+import express from 'express';
+import bodyParser from 'body-parser';
+import mongoose from 'mongoose';
+import { MONGODB_STR } from './config';
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+const app = express();
 
-var app = express();
-
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
-
-app.use(logger('dev'));
 app.use(express.json());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
-
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
+const mongoSettings = {
+  useFindAndModify: false,
+  useCreateIndex: true,
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+};
+mongoose.connect(MONGODB_STR, mongoSettings);
+mongoose.connection.on('open', () => {
+  // eslint-disable-next-line no-console
+  console.log('OK.');
+});
+mongoose.connection.on('error', (err) => {
+  // eslint-disable-next-line no-console
+  console.log(`SORRY. ${err}`);
+  process.exit(1);
 });
 
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
-
-module.exports = app;
+export default app;
